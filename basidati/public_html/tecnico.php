3500679da90af4087af62f227f3f9267acf51a8b
Giuseppe Merlino <Joe@MacBook-Pro-di-Giuseppe.local>
2015-08-24 11:26:23 +0200

    Area tecnico
    cambiata funzione

--- basidati/public_html/tecnico.php
+++ basidati/public_html/tecnico.php
@@ -5,9 +5,7 @@ $connect=connectDbServer();
 $db=selectDatabase($connect);
 echo page_link("Torna ad home page","index.php");
 if($_POST){
-	$query=mysql_query("SELECT Bicicletta FROM Colonnina WHERE CodiceMateriale=$_POST[col]");
-	$row=mysql_fetch_row($query);
-	if(add_operazione_tecnico($_POST['motivazione'],$_POST['col'],$row[0])) echo " Messaggio dal sistema: Riparazione rottura inserita!";
+	if(add_manutenzione($_POST['col'],$_POST['descrizione'])) echo " Messaggio dal sistema: Riparazione rottura inserita!";
 	  else echo " Messaggio dal sistema: Inserimento riparazione rottura non riuscito";
 }
 $query=mysql_query("SELECT CodiceMateriale,NomeStazione,COUNT(CodiceMateriale) FROM Colonnina RIGHT JOIN SegnalazioneRottura ON Colonnina.CodiceMateriale=SegnalazioneRottura.Colonnina GROUP BY CodiceMateriale ORDER BY COUNT(CodiceMateriale) DESC",$connect);
@@ -16,6 +14,6 @@ while($row=mysql_fetch_row($query)){
 
 	echo "<tr bgcolor='".dechex(16775492-4000*$row[2])."'><td><input type='radio' name='col' value='$row[0]'></td><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td>";
 	}
-echo "</table><textarea name='motivazione' maxlength='100' placeholder='Inserisci descrizione manutenzione eseguita' rows='5' cols='53'></textarea></br><input type='submit' name='riparazione' value='Riparazione Rottura'></form></div>";
+echo "</table><textarea name='descrizione' maxlength='100' placeholder='Inserisci descrizione manutenzione eseguita' rows='5' cols='53'></textarea></br><input type='submit' name='riparazione' value='Riparazione Rottura'></form></div>";
 page_end();
 ?>
