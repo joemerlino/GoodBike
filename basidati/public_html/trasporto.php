3db81dab8a57c86983fb782056c045ecb5b884c6
Giuseppe Merlino <merlino.giuseppe@hotmail.it>
2015-08-25 17:51:13 +0200

    Completato ufficialmente trasporto.php

--- basidati/public_html/trasporto.php
+++ basidati/public_html/trasporto.php
@@ -2,9 +2,18 @@
 require("library.php");
 page_start("Trasporto - BiciRent");
 echo "<script>
-var status;
-function set(valore){
-	if(valore)	document.getElementById('magazz').style.visibility = 'visible';
+var oldarrivo = 'Inizio';
+function set(valore,arrivo){
+	if(valore){
+		if(document.getElementById(oldarrivo)){
+			document.getElementById(oldarrivo).disabled = false;
+		}
+		oldarrivo = arrivo;	
+		document.getElementById('magazz').style.visibility = 'visible';
+		if(document.getElementById(arrivo)){
+			document.getElementById(arrivo).disabled = true;
+		}
+	}
 	else document.getElementById('magazz').style.visibility = 'hidden';
 }
 </script>
@@ -43,13 +52,13 @@ if($_POST){
 $query=mysql_query("SELECT Stazione.NomeStazione, COUNT(SegnalazioneMancanza.NomeStazione) FROM Stazione LEFT JOIN SegnalazioneMancanza ON Stazione.NomeStazione = SegnalazioneMancanza.NomeStazione GROUP BY SegnalazioneMancanza.NomeStazione ORDER BY COUNT(SegnalazioneMancanza.NomeStazione) DESC",$connect);
 echo "</br><form action='trasporto.php' method='POST'><div class='manutenzione'><h2>Monitoraggio segnalazioni mancanze</h2><table><thead><tr><th>Seleziona</th><th>Nome stazione</th><th>Priorità</th></tr></thead>";
 while($row=mysql_fetch_row($query)){
-	echo "<tr bgcolor='".dechex(16775492-4000*$row[1])."'><td><input type='radio' name='arrivo' value='$row[0]' onclick='set(true)'></td><td>".$row[0]."</td><td>".$row[1]."</td>";
+	echo "<tr bgcolor='".dechex(16775492-4000*$row[1])."'><td><input type='radio' name='arrivo' value='$row[0]' onclick=set(true,'$row[0]')></td><td>".$row[0]."</td><td>".$row[1]."</td>";
 	}
-	echo "<tr bgcolor='".dechex(16775492)."'><td><input type='radio' name='arrivo' value='Magazzino' onclick='set(false)'></td><td>Magazzino</td><td>-</td>";
+	echo "<tr bgcolor='".dechex(16776600)."'><td><input type='radio' name='arrivo' value='Magazzino' onclick=set(false,'Magazzino')></td><td>Magazzino</td><td>-</td>";
 echo "</table></div><div class='manutenzione'><h2>Riepilogo stazioni</h2><table><thead><tr><th>Seleziona</th><th>Codice Bicicletta</th><th>Stazione</th></tr></thead>";
 $query=mysql_query("SELECT Bicicletta, NomeStazione FROM Colonnina WHERE Bicicletta IS NOT NULL",$connect);
 while($row=mysql_fetch_row($query)){
-	echo "<tr><td><input type='checkbox' name='stazione[]' value='$row[0]'></td><td>".$row[0]."</td><td>".$row[1]."</td>";
+	echo "<tr><td><input type='checkbox' name='stazione[]' id='$row[1]' value='$row[0]'></td><td>".$row[0]."</td><td>".$row[1]."</td>";
 	}
 echo "</table></div><div class='manutenzione' id='magazz'><h2>Riepilogo Magazzino</h2><table>";
 $query=mysql_query("SELECT Bicicletta.CodiceMateriale FROM Bicicletta LEFT JOIN Materiale ON Bicicletta.CodiceMateriale = Materiale.CodiceMateriale WHERE Stato = 'InMagazzino' AND Danneggiato = FALSE",$connect);
